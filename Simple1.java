8dce37c944d68dc1322cade8d69a92f9e05e0a46
naveenkommisetti <98598674+naveenkommisetti@users.noreply.github.com>
2022-03-23 20:37:48 +0530

    Update Simple1.java

--- Simple1.java
+++ Simple1.java
@@ -1,6 +1,7 @@
 class Simple1{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-     System.out.println("Naveen Changes first");  
+     System.out.println("Naveen Changes first");
+     System.out.println("Naveen Changes first");
     }  
 }
